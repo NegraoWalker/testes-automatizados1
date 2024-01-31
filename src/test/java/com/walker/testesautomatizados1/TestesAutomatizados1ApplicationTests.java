0067bf66d61440e8ea5d3ddc7762db79015a760d
Walker Negrão <c0662240@vale.com>
2024-01-31 08:57:29 -0300

    Repositório criado

---
+++ src/test/java/com/walker/testesautomatizados1/TestesAutomatizados1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.testesautomatizados1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesAutomatizados1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
